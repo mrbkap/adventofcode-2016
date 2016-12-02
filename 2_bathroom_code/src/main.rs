c01677138a6697b38c4221aa38e3e9209bc63fa0
Blake Kaplan <mrbkap@gmail.com>
2016-12-01 21:49:51 -0800

    second star
    Plus some cleanups

--- 2_bathroom_code/src/main.rs
+++ 2_bathroom_code/src/main.rs
@@ -1,28 +1,34 @@
 use std::io;
 use std::io::BufRead;
 
+#[derive(Copy, Clone)]
 struct Position {
     x: usize,
     y: usize,
 }
 
 fn main() {
-    let keypad = vec![vec![1, 2, 3],
-                      vec![4, 5, 6],
-                      vec![7, 8, 9]];
-    let mut cur_pos = Position { x: 1, y: 1 };
+    let keypad = vec![vec!['\0', '\0', '1', '\0', '\0'],
+                      vec!['\0',  '2', '3',  '4', '\0'],
+                      vec![ '5',  '6', '7',  '8',  '9'],
+                      vec!['\0',  'A', 'B',  'C', '\0'],
+                      vec!['\0', '\0', 'D', '\0', '\0']];
+    let mut cur_pos = Position { x: 0, y: 2 };
     let stdin = io::stdin();
 
-    for wline in stdin.lock().lines() {
-        let uline = wline.unwrap();
-        let line = uline.trim();
+    for line in stdin.lock().lines().filter_map(|l| l.ok()).filter(|l| !l.is_empty()) {
         for inst in line.chars() {
+            let orig_pos = cur_pos;
             match inst {
                 'U' => if cur_pos.y > 0 { cur_pos.y -= 1; },
-                'D' => if cur_pos.y < 2 { cur_pos.y += 1; },
+                'D' => if cur_pos.y < keypad.len() - 1 { cur_pos.y += 1; },
                 'L' => if cur_pos.x > 0 { cur_pos.x -= 1; },
-                'R' => if cur_pos.x < 2 { cur_pos.x += 1; },
+                'R' => if cur_pos.x < keypad.len() - 1 { cur_pos.x += 1; },
                 _ => panic!("bad instruction {}", inst),
+            }
+
+            if keypad[cur_pos.y][cur_pos.x] == '\0' {
+                cur_pos = orig_pos;
             }
         }
 
