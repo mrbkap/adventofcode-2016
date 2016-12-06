7c77ea68353efb05277ea902853c5af78cd93c4a
Blake Kaplan <mrbkap@gmail.com>
2016-12-06 00:13:18 -1000

    use min_by_key

--- 6_santamessage/src/main.rs
+++ 6_santamessage/src/main.rs
@@ -2,12 +2,6 @@ use std::io;
 use std::io::BufRead;
 use std::collections::HashMap;
 
-fn char_for_map(map: &HashMap<char, u32>) -> char {
-    let mut vec : Vec<_> = map.iter().collect();
-    vec.sort_by_key(|e| e.1);
-    *vec.first().unwrap().0
-}
-
 fn main() {
     let mut frequences : Vec<HashMap<char, u32>> = Vec::new();
     let stdin = io::stdin();
@@ -21,6 +15,6 @@ fn main() {
         }
     }
 
-    let decoded : String = frequences.iter().map(|map| char_for_map(map)).collect();
+    let decoded : String = frequences.iter().map(|map| *map.iter().min_by_key(|&(_, c)| c).unwrap().0).collect();
     println!("{}", decoded);
 }
